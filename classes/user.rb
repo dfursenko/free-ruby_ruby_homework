d8247ec95197deb5bbacc712a50b8e6500563319
Dmitry Fursenko <dfursenko@gmail.com>
2018-06-26 16:03:33 +0700

    Create User class

---
+++ classes/user.rb
@@ -0,0 +1,15 @@
+##
+# User class
+class User
+  ##
+  # Return fio
+  def fio
+    'Yukihiro Matsumoto'
+  end
+
+  ##
+  # Return profession
+  def profession
+    'Computer scientist and software programmer'
+  end
+end
